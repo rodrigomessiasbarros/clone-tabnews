6a1cd32bfd49362775364149f2c8b25168844ee0
Rodrigo Messias Barros <rodrigomessiasbarros@gmail.com>
2023-08-29 17:28:23 -0300

    Adicionei emojis

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Carol, eu te amo muito e, se você me ama também, dê uma risada daquelas!</h1>
+    return <h1>💝 Carol, eu te amo muito e, se você me ama também, dê uma risada daquelas! 💝</h1>
 }
 
 export default Home;
