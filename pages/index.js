0afb1a386a7fdacfb1d6b67cdeca31c495baf5a3
Rodrigo Messias Barros <rodrigomessiasbarros@gmail.com>
2023-09-02 02:13:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>💝 Carol, eu te amo muito e, se você me ama também, dê uma risada daquelas! 💝</h1>
+function Home() {
+  return (
+    <h1>
+      💝 Carol, eu te amo muito e, se você me ama também, dê uma risada
+      daquelas! 💝
+    </h1>
+  );
 }
 
 export default Home;
